c52b211b779b450b12b2f6398eca8f2eb62b4398
hack3r-0m <oparikh113@gmail.com>
2021-05-31 04:47:34 +0530

    bumping to solidity v0.8.4

--- contracts/CestakingFarm.sol
+++ contracts/CestakingFarm.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
